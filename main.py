bbd58d6d013ac79184e02712551cc4c643759671
l1st1k <listik.mp@gmail.com>
2022-08-18 15:31:22 +0300

    Installed mongoengine, isort

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
 from dotenv import dotenv_values
+from fastapi import FastAPI
+from mongoengine import connect
 
 app = FastAPI()
 config = dotenv_values(".env")
+# connect(host=f"mongodb://{config['MONGO_USER']}:{config['MONGO_PASS']}@127.0.0.1:27017/{config['MONGO_DB']}?authSource=my_db")
 
 
 @app.get("/")
